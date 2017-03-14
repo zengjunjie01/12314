ec465a258acb343ff3a6f2623c151854110e8dcd
zengjunjie <346193354@qq.com>
2017-03-14 15:59:44 +0800

    base

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/3/14.
+ */
+(function (ag) {
+    ag.module("piModule")
+        .controller("piController",["$scope",
+            function ($scope) {
+
+            }])
+})(window.angular);
